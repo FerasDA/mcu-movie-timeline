2686af56d304996a06e6e0d40bde76f032a6a4c8
Feras Deiratany <fdeiratany@level20.com>
2020-05-02 13:32:22 -0400

    add virtical timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,6 +1,15 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+
+const movieCard = { background: "red", color: "white" }
+const showCard = { background: "blue", color: "white" }
+
 const Timeline = () => {
   const data = useStaticQuery(graphql`
     query allMCUData {
@@ -17,15 +26,22 @@ const Timeline = () => {
   `)
   return (
     <>
-      <h2>Timeline</h2>
-    <ul>
-      {data.allDataJson.edges.map(({ node, index }) => (
-        <li key={index}>{node.subTitle}: {node.title}</li>
-      ))}
-    </ul>
+      <div style={{ backgroundColor: "#000" }}>
+        <VerticalTimeline>
+          {data.allDataJson.edges.map(({ node }) => (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={node.type === "movie" ? movieCard : showCard}
+              date={node.subTitle}
+              iconStyle={node.type === "movie" ? movieCard : showCard}
+            >
+              <h3 className="vertical-timeline-element-title">{node.title}</h3>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   )
 }
 
 export default Timeline
-
